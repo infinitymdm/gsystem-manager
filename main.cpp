206dd81160000923d7c5815d5f70ab975cc4ee5c
Nathaniel <anzelknight@gmail.com>
2021-05-07 19:05:25 -0500

    Got glade ui file loading working.

--- main.cpp
+++ main.cpp
@@ -9,11 +9,22 @@ MainWindow::MainWindow() {
     set_default_size(300,200);
 }
 
+Glib::RefPtr<Gtk::Application> app;
+
+void load_ui() {
+    auto builder = Gtk::Builder::create_from_file("./glade/glade.ui");
+    Gtk::Window *window;
+    builder->get_widget("main_window", window);
+    app->add_window(*window);
+    window->show();
+}
+
 int main(int argc, char* argv[]) {
-    auto app = Gtk::Application::create("com.github.mdmellor.gsystem-manager");
-    auto window = MainWindow();
-    app->run(window);
-    return 0;
+
+    Glib::init();
+    app = Gtk::Application::create("com.github.mdmellor.gsystem-manager");
+    app->signal_activate().connect(sigc::ptr_fun(&load_ui));
+    return app->run(argc, argv);
 }
 
 // Compile with: gcc main.cpp -o main `pkg-config gtkmm-3.0 --cflags --libs` -lstdc++
